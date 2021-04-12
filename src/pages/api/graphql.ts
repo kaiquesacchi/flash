1b2d4d3a25cacfc43d843cf51cb8a60eb415eb51
Kaique Sacchi <sacchikaique@gmail.com>
2021-04-12 20:10:25 -0300

    Added the Apollo client/server boilerplate.

---
+++ src/pages/api/graphql.ts
@@ -0,0 +1,19 @@
+import { ApolloServer } from "apollo-server-micro";
+
+import { typeDefs } from "../../graphql/schemas";
+import { resolvers } from "../../graphql/resolvers";
+
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+// Lets the Apollo Server handle the parsing.
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+// Points the Apollo server to the endpoint.
+export default apolloServer.createHandler({ path: "/api/graphql" });
