a6069c9131d58c2e7a2e176616207b15d9efd08f
Kaique Sacchi <sacchikaique@gmail.com>
2021-04-13 15:20:40 -0300

    Enhanced the AddEmployee page design.

--- src/pages/addEmployee.tsx
+++ src/pages/addEmployee.tsx
@@ -3,7 +3,7 @@ import React, { useCallback, useEffect } from "react";
 import { APOLLO_STATE_PROP_NAME, initializeApollo } from "../../lib/apolloClient";
 import AppBar from "../components/AppBar/AppBar";
 import { GET_ALL_COMPANIES, iGetAllCompanies } from "../graphql/queries/company";
-import { Form, Input, Button, Select, message } from "antd";
+import { Form, Input, Button, Select, message, Space, Row, Col, Card, Divider } from "antd";
 import { useMutation, useQuery } from "@apollo/client";
 import { iCreateEmployee, MUTATION_CREATE_EMPLOYEE } from "../graphql/queries/employee";
 
@@ -28,55 +28,65 @@ export default function AddEmployee() {
     message.error("Unexpected error. Please, try again later...");
   }, [error]);
   return (
-    <div>
+    <Space size={20} direction="vertical" style={{ width: "100vw" }}>
       <AppBar />
-      <Form labelCol={{ span: 3 }} wrapperCol={{ span: 16 }} onFinish={handleSubmit} form={form}>
-        <Form.Item
-          label="First Name"
-          name="firstName"
-          rules={[{ required: true, message: "Please provide the employee's first name" }]}>
-          <Input />
-        </Form.Item>
-        <Form.Item
-          label="Last Name"
-          name="lastName"
-          rules={[{ required: true, message: "Please provide the employee's last name" }]}>
-          <Input />
-        </Form.Item>
-        <Form.Item label="CPF" name="cpf" rules={[{ required: true, message: "Please provide the employee's CPF" }]}>
-          <Input />
-        </Form.Item>
-        <Form.Item
-          label="Email"
-          name="email"
-          rules={[{ required: true, message: "Please provide the employee's email" }]}>
-          <Input />
-        </Form.Item>
-        <Form.Item
-          label="Employer"
-          name="companyID"
-          rules={[{ required: true, message: "Please select the employer" }]}>
-          <Select
-            showSearch
-            style={{ width: "100%" }}
-            placeholder="Please select"
-            options={data_allCompanies?.getAllCompanies.map((company) => ({
-              label: company.name,
-              value: company.id,
-            }))}
-            filterOption={(input, option) => {
-              const label = option?.label as string;
-              return label.toLowerCase().indexOf(input.toLowerCase()) >= 0;
-            }}
-          />
-        </Form.Item>
-        <Form.Item wrapperCol={{ offset: 3, span: 16 }}>
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
-        </Form.Item>
-      </Form>
-    </div>
+      <Row justify="center">
+        <Col xs={24} sm={18} md={18} lg={12} xl={12}>
+          <Card>
+            <Form onFinish={handleSubmit} form={form} layout="vertical">
+              <Form.Item
+                label="First Name"
+                name="firstName"
+                rules={[{ required: true, message: "Please provide the employee's first name" }]}>
+                <Input />
+              </Form.Item>
+              <Form.Item
+                label="Last Name"
+                name="lastName"
+                rules={[{ required: true, message: "Please provide the employee's last name" }]}>
+                <Input />
+              </Form.Item>
+              <Form.Item
+                label="CPF"
+                name="cpf"
+                rules={[{ required: true, message: "Please provide the employee's CPF" }]}>
+                <Input />
+              </Form.Item>
+              <Form.Item
+                label="Email"
+                name="email"
+                rules={[{ required: true, message: "Please provide the employee's email" }]}>
+                <Input />
+              </Form.Item>
+              <Form.Item
+                label="Employer"
+                name="companyID"
+                rules={[{ required: true, message: "Please select the employer" }]}>
+                <Select
+                  showSearch
+                  style={{ width: "100%" }}
+                  placeholder="Please select"
+                  options={data_allCompanies?.getAllCompanies.map((company) => ({
+                    label: company.name,
+                    value: company.id,
+                  }))}
+                  filterOption={(input, option) => {
+                    const label = option?.label as string;
+                    return label.toLowerCase().indexOf(input.toLowerCase()) >= 0;
+                  }}
+                />
+              </Form.Item>
+              <Divider />
+              <Form.Item>
+                <Button type="primary" htmlType="submit">
+                  Submit
+                </Button>
+              </Form.Item>
+            </Form>
+          </Card>
+        </Col>
+      </Row>
+    </Space>
   );
 }
 
