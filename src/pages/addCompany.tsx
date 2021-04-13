94c38db5300a76a25e4007dd089f43a536050104
Kaique Sacchi <sacchikaique@gmail.com>
2021-04-13 15:11:17 -0300

    Enhanced the AddCompany page design.

--- src/pages/addCompany.tsx
+++ src/pages/addCompany.tsx
@@ -1,7 +1,7 @@
 import React, { useCallback, useEffect } from "react";
 import AppBar from "../components/AppBar/AppBar";
 
-import { Form, Input, Button, Select, message } from "antd";
+import { Form, Input, Button, Select, message, Card, Divider, Row, Col, Space } from "antd";
 import { useMutation } from "@apollo/client";
 import { iCreateCompany, MUTATION_CREATE_COMPANY } from "../graphql/queries/company";
 
@@ -37,39 +37,58 @@ export default function addCompany() {
     message.error("Unexpected error. Please, try again later...");
   }, [error]);
   return (
-    <div>
+    <Space size={20} direction="vertical" style={{ width: "100vw" }}>
       <AppBar />
-      <Form labelCol={{ span: 3 }} wrapperCol={{ span: 16 }} onFinish={handleSubmit} form={form}>
-        <Form.Item label="Name" name="name" rules={[{ required: true, message: "Please provide the company's name" }]}>
-          <Input />
-        </Form.Item>
-        <Form.Item
-          label="Trading Name"
-          name="tradingName"
-          rules={[{ required: true, message: "Please provide the company's trading name" }]}>
-          <Input />
-        </Form.Item>
-        <Form.Item label="CNPJ" name="cnpj" rules={[{ required: true, message: "Please provide the company's CNPJ" }]}>
-          <Input />
-        </Form.Item>
-        <Form.Item
-          label="Address"
-          name="address"
-          rules={[{ required: true, message: "Please provide the company's address" }]}>
-          <Input />
-        </Form.Item>
-        <Form.Item
-          label="Benefits"
-          name="chosenBenefits"
-          rules={[{ required: true, message: "Please provide the company's chosen benefits" }]}>
-          <Select mode="multiple" allowClear style={{ width: "100%" }} placeholder="Please select" options={benefits} />
-        </Form.Item>
-        <Form.Item wrapperCol={{ offset: 3, span: 16 }}>
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
-        </Form.Item>
-      </Form>
-    </div>
+      <Row justify="center">
+        <Col xs={24} sm={18} md={18} lg={12} xl={12}>
+          <Card>
+            <Form onFinish={handleSubmit} form={form} layout="vertical">
+              <Form.Item
+                label="Name"
+                name="name"
+                rules={[{ required: true, message: "Please provide the company's name" }]}>
+                <Input />
+              </Form.Item>
+              <Form.Item
+                label="Trading Name"
+                name="tradingName"
+                rules={[{ required: true, message: "Please provide the company's trading name" }]}>
+                <Input />
+              </Form.Item>
+              <Form.Item
+                label="CNPJ"
+                name="cnpj"
+                rules={[{ required: true, message: "Please provide the company's CNPJ" }]}>
+                <Input />
+              </Form.Item>
+              <Form.Item
+                label="Address"
+                name="address"
+                rules={[{ required: true, message: "Please provide the company's address" }]}>
+                <Input />
+              </Form.Item>
+              <Form.Item
+                label="Benefits"
+                name="chosenBenefits"
+                rules={[{ required: true, message: "Please provide the company's chosen benefits" }]}>
+                <Select
+                  mode="multiple"
+                  allowClear
+                  style={{ width: "100%" }}
+                  placeholder="Please select"
+                  options={benefits}
+                />
+              </Form.Item>
+              <Divider />
+              <Form.Item>
+                <Button type="primary" htmlType="submit">
+                  Submit
+                </Button>
+              </Form.Item>
+            </Form>
+          </Card>
+        </Col>
+      </Row>
+    </Space>
   );
 }
