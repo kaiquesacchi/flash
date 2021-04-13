8402d7eee88e0468fdd53a218d047671b9fa5c52
Kaique Sacchi <sacchikaique@gmail.com>
2021-04-13 17:28:24 -0300

    Refactored the AppBar component.

--- src/components/AppBar/AppBar.tsx
+++ src/components/AppBar/AppBar.tsx
@@ -1,31 +1,50 @@
+// React and NextJS.
 import React, { useCallback } from "react";
-import { Menu } from "antd";
-
-import { SearchOutlined, HomeOutlined, UserOutlined } from "@ant-design/icons";
 import { useRouter } from "next/dist/client/router";
+import { MenuInfo } from "rc-menu/lib/interface";
+
+// Ant Design.
+import { Menu } from "antd";
+import { SearchOutlined, HomeOutlined, UserOutlined } from "@ant-design/icons";
+
+const routes = [
+  {
+    key: "/",
+    label: "Search Employees",
+    icon: <SearchOutlined />,
+  },
+  {
+    key: "/newCompany",
+    label: "New Company",
+    icon: <HomeOutlined />,
+  },
+  {
+    key: "/newEmployee",
+    label: "New Employee",
+    icon: <UserOutlined />,
+  },
+];
 
 export default function AppBar() {
   const router = useRouter();
   const currentRoute = router.route;
+
+  // Handlers.
   const handleClick = useCallback(
-    (event) => {
+    (event: MenuInfo) => {
       if (event.key === currentRoute) return;
-      router.replace(event.key);
+      router.replace(event.key as string);
     },
     [router]
   );
 
   return (
     <Menu onClick={handleClick} selectedKeys={[currentRoute]} mode="horizontal">
-      <Menu.Item key="/" icon={<SearchOutlined />}>
-        Search Employees
-      </Menu.Item>
-      <Menu.Item key="/newCompany" icon={<HomeOutlined />}>
-        New Company
-      </Menu.Item>
-      <Menu.Item key="/newEmployee" icon={<UserOutlined />}>
-        New Employee
-      </Menu.Item>
+      {routes.map(({ key, label, icon }) => (
+        <Menu.Item key={key} icon={icon}>
+          {label}
+        </Menu.Item>
+      ))}
     </Menu>
   );
 }
