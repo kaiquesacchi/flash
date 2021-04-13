bc6ce7d8737112963954d5ea7fc74d986603e6c4
Kaique Sacchi <sacchikaique@gmail.com>
2021-04-13 13:48:35 -0300

    Added AppBar component.

---
+++ src/components/AppBar/AppBar.tsx
@@ -0,0 +1,31 @@
+import React, { useCallback } from "react";
+import { Menu } from "antd";
+
+import { SearchOutlined, HomeOutlined, UserOutlined } from "@ant-design/icons";
+import { useRouter } from "next/dist/client/router";
+
+export default function AppBar() {
+  const router = useRouter();
+  const currentRoute = router.route;
+  const handleClick = useCallback(
+    (event) => {
+      if (event.key === currentRoute) return;
+      router.replace(event.key);
+    },
+    [router]
+  );
+
+  return (
+    <Menu onClick={handleClick} selectedKeys={[currentRoute]} mode="horizontal">
+      <Menu.Item key="/" icon={<SearchOutlined />}>
+        Search Employees
+      </Menu.Item>
+      <Menu.Item key="/addCompany" icon={<HomeOutlined />}>
+        New Company
+      </Menu.Item>
+      <Menu.Item key="/addEmployee" icon={<UserOutlined />}>
+        New Employee
+      </Menu.Item>
+    </Menu>
+  );
+}
