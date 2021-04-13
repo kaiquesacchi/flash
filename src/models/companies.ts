8244fc0d3bbea08a66b2f0ff66bccdd70854dcb9
Kaique Sacchi <sacchikaique@gmail.com>
2021-04-13 17:59:20 -0300

    Updated the models' error messages.

--- src/models/companies.ts
+++ src/models/companies.ts
@@ -3,22 +3,22 @@ import mongoose from "mongoose";
 const CompanySchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, "Please provide a name to the company"],
+    required: [true, "Please provide the company's name"],
     maxlength: [255, "Name cannot have more than 255 characters"],
   },
   tradingName: {
     type: String,
-    required: [true, "Please provide a trading-name to the company"],
-    maxlength: [255, "Trading-name cannot have more than 255 characters"],
+    required: [true, "Please provide the company's trading name"],
+    maxlength: [255, "Trading name cannot have more than 255 characters"],
   },
   cnpj: {
     type: String,
-    required: [true, "Please provide a CNPJ to the company"],
+    required: [true, "Please provide the company's CNPJ"],
     maxlength: [255, "CNPJ cannot have more than 255 characters"],
   },
   address: {
     type: String,
-    required: [true, "Please provide an address to the company"],
+    required: [true, "Please provide the company's address"],
     maxlength: [255, "Address cannot have more than 255 characters"],
   },
   chosenBenefits: {
--- src/models/employees.ts
+++ src/models/employees.ts
@@ -3,28 +3,28 @@ import mongoose from "mongoose";
 const EmployeeSchema = new mongoose.Schema({
   firstName: {
     type: String,
-    required: [true, "Please provide a first name to the employee"],
+    required: [true, "Please provide the employee's first name"],
     maxlength: [255, "First name cannot have more than 255 characters"],
   },
   lastName: {
     type: String,
-    required: [true, "Please provide a last name to the employee"],
+    required: [true, "Please provide the employee's last name"],
     maxlength: [255, "Last name cannot have more than 255 characters"],
   },
   cpf: {
     type: String,
-    required: [true, "Please provide a CPF to the employee"],
+    required: [true, "Please provide the employee's CPF"],
     maxlength: [255, "CPF cannot have more than 255 characters"],
   },
   email: {
     type: String,
-    required: [true, "Please provide an email to the employee"],
+    required: [true, "Please provide the employee's email"],
     maxlength: [255, "Email cannot have more than 255 characters"],
   },
   companyID: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Company",
-    required: [true, "Please provide the employee's employer id"],
+    required: [true, "Please provide the employee's employer ID"],
   },
 });
 
