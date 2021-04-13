6b942fa7dc83851fca13f3de8a19c1cd08fcddfd
Kaique Sacchi <sacchikaique@gmail.com>
2021-04-13 18:59:36 -0300

    Refactored the Home Page. Added error messages. Reorganized imports.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,29 @@
-import { useLazyQuery, useQuery } from "@apollo/client";
-import { GetServerSideProps } from "next";
+// React and NextJS.
 import React, { useEffect, useState } from "react";
+import Head from "next/head";
+import { GetServerSideProps } from "next";
+
+// ApolloClient and GraphQL.
+import { useLazyQuery, useQuery } from "@apollo/client";
 import { APOLLO_STATE_PROP_NAME, initializeApollo } from "../../lib/apolloClient";
 import { GET_ALL_COMPANIES, iGetAllCompanies } from "../graphql/queries/company";
 import { GET_EMPLOYEES_BY_COMPANY, iEmployee, iGetEmployeesByCompany } from "../graphql/queries/employee";
+
+// Ant Design.
+import { Select, Card, Divider, Space, Row, Col, message } from "antd";
+
+// Components.
 import EmployeeTable from "../components/EmployeeTable/EmployeeTable";
-import { Select, Card, Divider, Space, Row, Col } from "antd";
 import AppBar from "../components/AppBar/AppBar";
-import Head from "next/head";
 
 export default function Home() {
   const { data: data_allCompanies, error: error_allCompanies } = useQuery<iGetAllCompanies>(GET_ALL_COMPANIES);
   const [getEmployees, { data: data_employees, error: error_employees }] = useLazyQuery<iGetEmployeesByCompany>(
     GET_EMPLOYEES_BY_COMPANY
   );
-
   const [employees, setEmployees] = useState<iEmployee[]>([]);
+
+  // Handlers.
   const handleSelect = (value: string) => {
     getEmployees({
       variables: {
@@ -24,9 +32,19 @@ export default function Home() {
     });
   };
 
+  // UseEffects.
   useEffect(() => {
     setEmployees(data_employees?.getEmployeesByCompany || []);
   }, [data_employees]);
+
+  useEffect(() => {
+    if (error_allCompanies) {
+      message.error("Couldn't load the companies list. Please try again later...");
+    }
+    if (error_employees) {
+      message.error("Couldn't load the companies' employee list. Please try again later...");
+    }
+  }, [error_allCompanies, error_employees]);
 
   return (
     <>
@@ -68,7 +86,7 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     await apolloClient.query<iGetAllCompanies>({
       query: GET_ALL_COMPANIES,
     });
-  } catch {}
+  } catch {} // If the query fails, the error will be added to the cache, so there is no need to handle it here.
   return {
     props: { [APOLLO_STATE_PROP_NAME]: apolloClient.cache.extract() },
   };
